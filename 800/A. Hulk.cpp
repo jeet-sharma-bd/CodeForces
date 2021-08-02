da4900e9bcfd3d57c13c297bfe20ea1ac1d23baf
Sree Prosenzit Kumar Sharma <79397907+jeet-sharma-bd@users.noreply.github.com>
2021-08-02 15:42:47 +0600

    A. Hulk
    Dr. Bruce Banner hates his enemies (like others don't). As we all know, he can barely talk when he turns into the incredible Hulk. That's why he asked you to help him to express his feelings.
    
    Hulk likes the Inception so much, and like that his feelings are complicated. They have n layers. The first layer is hate, second one is love, third one is hate and so on...
    
    For example if n = 1, then his feeling is "I hate it" or if n = 2 it's "I hate that I love it", and if n = 3 it's "I hate that I love that I hate it" and so on.
    
    Please help Dr. Banner.
    
    Input
    The only line of the input contains a single integer n (1 ≤ n ≤ 100) — the number of layers of love and hate.
    
    Output
    Print Dr.Banner's feeling in one line.
    
    Examples
    input
    1
    outputCopy
    I hate it
    input
    2
    output
    I hate that I love it
    input
    3
    output
    I hate that I love that I hate it

---
+++ 800/A. Hulk.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n;
+int a,c=1,i=1;
+cin>>n;
+int t=n;
+while(i<=n){
+	if(i>=1 and i<= n){
+		cout<<"I hate";
+		i++;
+		c++;				
+	} if(i>=2 and i<= n){		
+		cout<<" that I love ";
+		i++;
+		c++;
+	}
+	if(t>=c){
+			cout<<" that ";
+		}
+	
+}
+cout<<" it "<<endl;
+}
