e300c3ba03d15e7e8fa16a76fdb0670eaf6e6d9c
Sree Prosenzit Kumar Sharma <79397907+jeet-sharma-bd@users.noreply.github.com>
2021-08-20 11:20:30 +0600

    A. Equation
    https://codeforces.com/problemset/problem/1269/A
    
    Let's call a positive integer composite if it has at least one divisor other than 1 and itself. For example:
    
    the following numbers are composite: 1024, 4, 6, 9;
    the following numbers are not composite: 13, 1, 2, 3, 37.
    You are given a positive integer n. Find two composite integers a,b such that a−b=n.
    
    It can be proven that solution always exists.
    
    Input
    The input contains one integer n (1≤n≤107): the given integer.
    
    Output
    Print two composite integers a,b (2≤a,b≤109,a−b=n).
    
    It can be proven, that solution always exists.
    
    If there are several possible solutions, you can print any.
    
    Examples
    input
    1
    output
    9 8
    input
    512
    output
    4608 4096

---
+++ 800/A. Equation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+long long n;
+	cin >> n;
+	
+	if (n%2 == 0){
+		cout<< n+4 <<" "<<4<< endl;
+	} 
+	else{
+		cout<<n+9<<" "<<9<< endl;
+	}
+}
+ 
